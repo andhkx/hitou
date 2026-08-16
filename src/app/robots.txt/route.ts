0ac6a77480b7a1a4f3b3b099723aef3bbf28136f
Andhika Andriana Putra <andhikaputrach273@gmail.com>
2026-08-16 10:28:06 +0700

    update: route.ts

--- src/app/robots.txt/route.ts
+++ src/app/robots.txt/route.ts
@@ -2,7 +2,7 @@ export const dynamic = "force-static";
 
 export function GET() {
   const body =
-    "User-agent: *\nAllow: /\n\nSitemap: https://hitoustudio.pages.dev/sitemap.xml";
+    "User-agent: *\nAllow: /\n\nSitemap: https://hitou.my.id/sitemap.xml";
   return new Response(body, {
     headers: { "Content-Type": "text/plain" },
   });
